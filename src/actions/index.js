6b83d9f64272cc43332a052f570d0e6e9759309f
Markeisha <markeisha94@gmail.com>
2022-01-21 12:04:19 -0600

    finished adding action and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,33 @@ import axios from 'axios';
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
 //3. Add a standard action that allows us to set the value of the error message slice of state.
+
+export const FETCH_START = 'FETCH_START'
+export const FETCH_SUCCESS = 'FETCH_SUCCESS'
+export const ADD_SMURF = 'ADD_SMURF' 
+export const FETCH_ERROR = 'FETCH_ERROR'
+
+export const fetchSmurfs = (dispatch) => {
+    dispatch({ type: FETCH_START })
+    axios.get('http://localhost:3333/smurfs')
+        .then((res) => {
+            dispatch({ type: FETCH_SUCCESS, payload: res.data })
+        })
+        .catch((err) => {
+            dispatch({ type: FETCH_ERROR, payload: err.message })
+        })
+};
+
+export const addSmurf = (smurf) => {
+    return ({
+        type: ADD_SMURF,
+        payload: smurf
+    })
+}
+
+export const fetchError = (err) => {
+    return ({
+        type: FETCH_ERROR,
+        payload: err
+    })
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,39 @@
+import { START_FETCH, FETCH_SUCCESS, FETCH_ERROR, ADD_SMURF } from '../actions';
 
 export const initialState = {
+   smurfs: [],
+   isLoading: false,
+   error: ''
 }
 
-const reducer = ()=>{
-}
+const reducer = (state = initialState, action)=>{
+    switch(action.type) {
+        case START_FETCH: 
+            return {
+                ...state,
+                isLoading: true
+            }
+        case FETCH_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                smurfs: action.payload
+            }
+        case FETCH_ERROR:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload
+            }
+        case ADD_SMURF:
+            return {
+                ...state,
+                isLoading: false,
+                smurfs: [...state.smurfs, action.payload]
+            }
+        default:
+            return state;
+};
 
 //**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
 export default reducer;
